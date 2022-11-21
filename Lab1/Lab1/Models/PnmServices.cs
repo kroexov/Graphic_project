7e4707fe6917c8a9bd3ee8a64e5e09d78e842da1
deworldgreen123 <71402649+deworldgreen123@users.noreply.github.com>
2022-11-21 22:59:52 +0300

    add AssignGamma and ConvertGamma

--- Lab1/Lab1/Models/PnmServices.cs
+++ Lab1/Lab1/Models/PnmServices.cs
@@ -34,6 +34,22 @@ public class PnmServices: IPnmServices
         return RefreshImage();
     }
 
+    public void AssignGamma(double newGamma)
+    {
+        if (_fileImg != null)
+        {
+            _fileImg.SetGammaCoeffficent(newGamma);
+        }
+    }
+
+    public void ConvertGamma(double newGamma)
+    {
+        if (_fileImg != null)
+        {
+            _fileImg.ConvertGammaImage(newGamma);
+        }
+    }
+
     public byte[] SaveFile()
     {
         return _fileImg.SaveFile(_bytes);
