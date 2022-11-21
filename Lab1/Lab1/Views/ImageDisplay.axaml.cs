d26783e70304cef698f07f17fedd80684cd3697a
Kroexov <kroexov@gmail.com>
2022-11-21 23:18:03 +0300

    test commit for lab-4

--- Lab1/Lab1/Views/ImageDisplay.axaml.cs
+++ Lab1/Lab1/Views/ImageDisplay.axaml.cs
@@ -1,18 +1,36 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace Lab1.Views;
 
 public partial class ImageDisplay : UserControl
 {
+    private bool _firstTime = true;
+    private double _x;
+    private double _y;
     public ImageDisplay()
     {
         InitializeComponent();
     }
 
-    private void InitializeComponent()
+    private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
     {
-        AvaloniaXamlLoader.Load(this);
+        if (_firstTime)
+        {
+            _firstTime = false;
+            var point = e.GetCurrentPoint(sender as Image);
+            _x = point.Position.X;
+            _y = point.Position.Y;
+        }
+
+        else
+        {
+            var point = e.GetCurrentPoint(sender as Image);
+            //DrawFunction(_x, _y, point.Position.X, point.Position.Y);
+        }
+        
     }
 }
